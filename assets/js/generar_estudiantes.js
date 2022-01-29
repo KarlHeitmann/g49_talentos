cbf64e3324abfec3ef9e4a04dcf2696808edc9e9
Karl Heitmann <karlheitmann@Karls-MacBook-Pro.local>
2022-01-29 11:49:05 -0300

    avances en creacion de cards

--- assets/js/generar_estudiantes.js
+++ assets/js/generar_estudiantes.js
@@ -1,10 +1,13 @@
+/* eslint-disable dot-notation */
+/* eslint-disable comma-dangle */
+
 const estudiantes = [
-  { imagen: "./assets/img/michel_valenzuela.jpg", nombre: "Michel", habilidades: "etc 1" },
-  { imagen: "./assets/img/chancho.jpg", nombre: "Marietta", habilidades: "etc 2" },
-  { imagen: "./assets/img/chancho.jpg", habilidades: "etc 3", nombre: "Yo" },
-  { imagen: "./assets/img/medium_yo.jpg", habilidades: "etc 4", nombre: "Cristian", fondo_oscuro: false },
-  { imagen: "./assets/img/medium_yo.jpg", habilidades: "etc 4", nombre: "Cristian", fondo_oscuro: true },
-  { imagen: "./assets/img/medium_yo.jpg", habilidades: ["Technical Analysis", "Design", "Python", "HTML & CSS"], nombre: "Cristian" },
+  { imagen: './assets/img/michel_valenzuela.jpg', nombre: 'Michel', habilidades: 'etc 1' },
+  { imagen: './assets/img/chancho.jpg', nombre: 'Marietta', habilidades: 'etc 2' },
+  { imagen: './assets/img/chancho.jpg', habilidades: 'etc 3', nombre: 'Yo' },
+  { imagen: './assets/img/medium_yo.jpg', habilidades: 'etc 4', nombre: 'Cristian', fondo_oscuro: false },
+  { imagen: './assets/img/medium_yo.jpg', habilidades: 'etc 4', nombre: 'Cristian', fondo_oscuro: true },
+  { imagen: './assets/img/medium_yo.jpg', habilidades: ['Technical Analysis', 'Design', 'Python', 'HTML & CSS'], nombre: 'Cristian' },
 ]
 
 /* equivalente en lenguaje ruby.
@@ -13,22 +16,29 @@ def nombre_de_funcion(argumento1, argumento2)
 end
 */
 
-function nombre_de_funcion(argumento1, argumento2) {
-  //cuerpo de la funcion
-}
+// eslint-disable-next-line camelcase
 const contenedor_talentos = document.querySelector('#contenedor-talentos')
 estudiantes.forEach(estudiante => {
-  console.log("Estudiante: ", estudiante)
-  console.log("Nombre estudiante: ", estudiante["nombre"])
-  let imagen = document.createElement('img')
-  imagen.setAttribute('src', estudiante["imagen"])
-  contenedor_talentos.appendChild(imagen)
-  let nombre = document.createElement('h5')
-  // nombre.innerText = estudiante["nombre"]
-  nombre.innerHTML = `<p>nombre: <strong>${estudiante["nombre"]}</strong></p>`
-  contenedor_talentos.appendChild(nombre)
-});
+  // eslint-disable-next-line camelcase
+  const card_estudiante = generarTarjetaEstudiante(estudiante)
+  contenedor_talentos.appendChild(card_estudiante)
+})
 
+function generarTarjetaEstudiante (estudiante) {
+  const col = document.createElement('div')
+  col.setAttribute('class', 'col-sm-4')
+  const card = document.createElement('div')
+  card.setAttribute('class', 'card')
+  const imagen = document.createElement('img')
+  imagen.setAttribute('src', estudiante['imagen'])
+  card.appendChild(imagen)
+  const nombre = document.createElement('h5')
+  // nombre.innerText = estudiante["nombre"]
+  nombre.innerHTML = `<p>nombre: <strong>${estudiante['nombre']}</strong></p>`
+  card.appendChild(nombre)
+  col.appendChild(card)
+  return col
+}
 
 // console.log(document.querySelector('#contenedor-talentos'))
 // console.log(document.querySelectorAll('.card'))
